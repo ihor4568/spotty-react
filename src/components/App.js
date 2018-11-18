009c48ecb5ee3dd4e1dfe140d7194b2bf0aa0591
IYeskov <IgorYeskov92@gmail.com>
2018-11-18 22:19:32 +0200

    add Albums.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
-import styled from "styled-components/macro";
 
-const Container = styled.div`
-  border: 1px solid black;
-  padding: ${props => props.padding || "20px"};
-`;
+import Albums from "./Albums";
 
 export default class App extends Component {
   render() {
     return (
-      <Container>
-        <div>Test</div>
-      </Container>
+      <>
+        <Albums />
+      </>
     );
   }
 }

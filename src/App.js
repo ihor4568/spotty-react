eb077882799f865b42d7041e76515594d2b494da
Oleg Shynkarenko <oleg.shynkarenko1985@gmail.com>
2018-11-19 12:00:36 +0200

    KHWE-52: Add basic view

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PlayerContainer from './player/PlayerContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PlayerContainer />
+      </div>
+    );
+  }
+}
+
+export default App;

487f503e87401a58dcc63825713005d043e92002
Ihor Tsarenko <tsarenko.igor1@gmail.com>
2018-11-22 11:16:58 +0200

    KHWE-51 add firebase config

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,7 @@
 import React, { Component } from "react";
-import styled from "styled-components/macro";
-
-const Container = styled.div`
-  border: 1px solid black;
-  padding: ${props => props.padding || "20px"};
-`;
 
 export default class App extends Component {
   render() {
-    return (
-      <Container>
-        <div>Test</div>
-      </Container>
-    );
+    return <div>Test</div>;
   }
 }

d7b8daa2d8e1ca8228c84bf1c26f9f4e7fc5786b
Oleg Shynkarenko <oleg.shynkarenko1985@gmail.com>
2018-11-22 13:22:10 +0200

    KHWE-52: Fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import styled from "styled-components/macro";
 import PlayerContainer from "./player/PlayerContainer";
-
-const Container = styled.div``;
 
 export default class App extends Component {
   render() {
     return (
-      <Container>
+      <div>
         <PlayerContainer />
-      </Container>
+      </div>
     );
   }
 }

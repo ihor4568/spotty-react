3941fdd8f965a2339ae4a3871c4a9daa3a1e367d
IYeskov <IgorYeskov92@gmail.com>
2018-11-27 13:26:27 +0200

    fix

--- src/components/MySongs.js
+++ src/components/MySongs.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 function MySongs(props) {
-  return <h1>My Songs</h1>;
+  return (
+    <Typography variant="h4" component="h2">
+      My Songs
+    </Typography>
+  );
 }
 
 export default MySongs;

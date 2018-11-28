d42c68bf64fb1d8340d48c5f5d9ae8c013747de1
Pavel Baranchuk <pbaranchuk@gmail.com>
2018-11-28 19:00:23 +0200

    KHWE-63: Change initialState variable

--- src/store/reducers/artists.js
+++ src/store/reducers/artists.js
@@ -1,8 +1,8 @@
 import { ADD_ARTISTS } from "../actionTypes";
 
-const initialState = [];
+const INITIAL_STATE = [];
 
-export default function artistsReducer(state = initialState, action) {
+export default function artistsReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case ADD_ARTISTS:
       return action.payload;

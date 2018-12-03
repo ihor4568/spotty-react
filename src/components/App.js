14b4f1a66f97b8333a2ed0d6ba9eb5bd312be75c
Ivan Yevsiutin <gamesminer123@gmail.com>
2018-12-03 12:45:13 +0200

    Small fix

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import theme from "../theme";
 import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 
-import MySongs from "./mySongs/MySongsTable";
+import MySongsTable from "./mySongs/MySongsTable";
 import Artists from "./artists/Artists";
 import About from "./about/About";
 import Albums from "./albums/Albums";
@@ -52,7 +52,7 @@ class App extends Component {
             <Switch>
               <PublicRoute exact path="/login" component={Auth} />
               <Redirect exact from="/" to="/albums" />
-              <PrivateRoute exact path="/mysongs" component={MySongs} />
+              <PrivateRoute exact path="/mysongs" component={MySongsTable} />
               <PrivateRoute exact path="/albums" component={Albums} />
               <PrivateRoute exact path="/artists" component={Artists} />
               <PrivateRoute path="/about" component={About} />

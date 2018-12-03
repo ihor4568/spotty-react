796518913f0ecc64377765c542ef2a357b4d2af4
Liubov Gribiniyk <lubovgribiniyk@gmail.com>
2018-12-03 11:24:23 +0200

    KHWE-86: Add error handling for auth form

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -3,3 +3,5 @@ export const ADD_ALBUMS = "ADD_ALBUMS";
 export const USER_FETCHED = "USER_FETCHED";
 export const USER_NOT_FOUND = "USER_NOT_FOUND";
 export const SIGN_OUT = "SIGN_OUT";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const CLEAR_AUTH_ERROR = "CLEAR_AUTH_ERROR";

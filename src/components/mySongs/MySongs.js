c0e692700e8180d0a03d5f953e3dafb6e92eea57
Ivan Yevsiutin <gamesminer123@gmail.com>
2018-12-08 02:35:25 +0200

    Small fix

--- src/components/mySongs/MySongs.js
+++ src/components/mySongs/MySongs.js
@@ -2,24 +2,25 @@ import React, { Component } from "react";
 import Title from "../shared/Title";
 import TableLayout from "../shared/TableLayout";
 import PropTypes from "prop-types";
-import { loadSongs } from "../../store/actionCreators/songs";
+import { getUserSongs } from "../../store/actionCreators/user";
 import { connect } from "react-redux";
 
 class MySongs extends Component {
   static propTypes = {
-    loadSongs: PropTypes.func,
-    songs: PropTypes.array.isRequired
+    userSongs: PropTypes.array.isRequired,
+    auth: PropTypes.object.isRequired,
+    getUserSongs: PropTypes.func
   };
 
   componentDidMount() {
-    this.props.loadSongs("album1");
+    this.props.getUserSongs(this.props.auth.user.uid);
   }
 
   render() {
     return (
       <>
         <Title name="My Songs" />
-        <TableLayout songs={this.props.songs} />
+        <TableLayout songs={this.props.userSongs} />
       </>
     );
   }
@@ -27,12 +28,13 @@ class MySongs extends Component {
 
 function mapStateToProps(state) {
   return {
-    songs: state.songs
+    userSongs: state.userSongs,
+    auth: state.auth
   };
 }
 
 const mapDispatchToProps = {
-  loadSongs
+  getUserSongs
 };
 
 export default connect(

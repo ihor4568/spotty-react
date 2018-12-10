b63e9792f3aa074489a37c04f5d6dc61ae365128
Ihor Tsarenko <tsarenko.igor1@gmail.com>
2018-12-10 13:10:02 +0200

    KHWE-71 minor changes

--- src/store/actionCreators/shareView.js
+++ src/store/actionCreators/shareView.js
@@ -4,18 +4,18 @@ import { MusicService } from "../../services/MusicService";
 export function getSong(id) {
   return async dispatch => {
     try {
-      dispatch({ type: actionTypes.ADD_SONG_START });
+      dispatch({ type: actionTypes.GET_SONG_START });
       const song = await MusicService.getSong(id);
       dispatch(addSong(song));
     } catch (e) {
-      dispatch({ type: actionTypes.ADD_SONG_FAIL });
+      dispatch({ type: actionTypes.GET_SONG_FAIL });
     }
   };
 }
 
 export function addSong(payload) {
   return {
-    type: actionTypes.ADD_SONG_SUCCESS,
+    type: actionTypes.GET_SONG_SUCCESS,
     payload
   };
 }

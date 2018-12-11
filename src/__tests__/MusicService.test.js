878533039a0e0901e24d7661778c3cd1cfba44a3
WOLFRIEND <wolfriend777@gmail.com>
2018-12-11 21:37:11 +0300

    Finish tests

--- src/__tests__/MusicService.test.js
+++ src/__tests__/MusicService.test.js
@@ -2,6 +2,7 @@ import faker from "faker";
 
 import {
   data as expectedResult,
+  snapshot,
   FirebaseService
 } from "../services/FirebaseService";
 
@@ -83,6 +84,77 @@ describe("Music Service", () => {
       await MusicService.getAlbumSongs(randomId);
       expect(database.ref).toHaveBeenCalledWith(`albums/${randomId}`);
       expect(database.once).toHaveBeenCalledWith("value");
+    });
+  });
+
+  describe("getUserSongs", () => {
+    const returnValue = ["song1"];
+    const prev = snapshot.val;
+
+    afterAll(() => {
+      snapshot.val = prev;
+    });
+
+    it("should return an array with proper data", async () => {
+      snapshot.val = () => returnValue;
+      const expected = [returnValue];
+      const result = await MusicService.getUserSongs(randomId);
+      expect(result).toEqual(expect.arrayContaining(expected));
+    });
+
+    it("should return empty array if there is no data", async () => {
+      snapshot.val = () => null;
+      const result = await MusicService.getUserSongs(randomId);
+      expect(result).toEqual([]);
+    });
+
+    it("should call firebase function with proper arguments", async () => {
+      snapshot.val = () => returnValue;
+      await MusicService.getUserSongs(randomId);
+      expect(database.ref).toHaveBeenCalledWith(`users/${randomId}/songs`);
+      expect(database.once).toHaveBeenCalledWith("value");
+    });
+  });
+
+  describe("setUserSong", () => {
+    const returnValue = ["song1", "song2"];
+    const prev = snapshot.val;
+
+    afterAll(() => {
+      snapshot.val = prev;
+    });
+
+    it("should call firebase function with proper arguments", async () => {
+      snapshot.val = () => returnValue;
+      await MusicService.setUserSong(randomId, "song3");
+      expect(database.ref).toHaveBeenCalledWith(`users/${randomId}/songs`);
+      expect(database.once).toHaveBeenCalledWith("value");
+    });
+
+    it("should call firebase function with proper arguments if there is no given song as the argument yet", async () => {
+      snapshot.val = () => returnValue;
+      await MusicService.setUserSong(randomId, "song3");
+      expect(database.ref).toHaveBeenCalledWith(`users/${randomId}/songs/2`);
+      expect(database.set).toHaveBeenCalledWith("song3");
+    });
+
+    it("should call firebase function with proper arguments if once function's promise returns null", async () => {
+      snapshot.val = () => null;
+      await MusicService.setUserSong(randomId, "song1");
+      expect(database.ref).toHaveBeenCalledWith(`users/${randomId}/songs/0`);
+      expect(database.set).toHaveBeenCalledWith("song1");
+    });
+  });
+
+  describe("removeUserSong", () => {
+    it("should call firebase function with proper arguments", async () => {
+      const prev = snapshot.val;
+      snapshot.val = () => ["song1", "song2"];
+      await MusicService.removeUserSong(randomId, "song1");
+      expect(database.ref).toHaveBeenCalledWith(`users/${randomId}/songs`);
+      expect(database.once).toHaveBeenCalledWith("value");
+      expect(database.set).toHaveBeenCalledWith(["song2"]);
+      snapshot.val = prev;
     });
   });
 });

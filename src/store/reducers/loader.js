af58b5110d18e8136b31aed74365dc4596221acd
WOLFRIEND <wolfriend777@gmail.com>
2018-12-12 19:28:44 +0300

    Fix comments

--- src/store/reducers/loader.js
+++ src/store/reducers/loader.js
@@ -5,21 +5,21 @@ export const INITIAL_STATE = false;
 export default function loaderReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case actionTypes.FETCH_USER_SONGS_START:
-    case actionTypes.FETCH_USER_SONGS_FAIL:
     case actionTypes.ADD_ALBUMS_START:
-    case actionTypes.ADD_ALBUMS_FAIL:
     case actionTypes.ADD_ARTISTS_START:
-    case actionTypes.ADD_ARTISTS_FAIL:
     case actionTypes.FETCH_ALBUM_SONGS_START:
-    case actionTypes.FETCH_ALBUM_SONGS_FAIL:
     case actionTypes.FETCH_ARTISTS_SONGS_START:
-    case actionTypes.FETCH_ARTISTS_SONGS_FAIL:
       return true;
     case actionTypes.FETCH_USER_SONGS_SUCCESS:
+    case actionTypes.FETCH_USER_SONGS_FAIL:
     case actionTypes.ADD_ALBUMS_SUCCESS:
+    case actionTypes.ADD_ALBUMS_FAIL:
     case actionTypes.ADD_ARTISTS_SUCCESS:
+    case actionTypes.ADD_ARTISTS_FAIL:
     case actionTypes.FETCH_ALBUM_SONGS_SUCCESS:
+    case actionTypes.FETCH_ALBUM_SONGS_FAIL:
     case actionTypes.FETCH_ARTISTS_SONGS_SUCCESS:
+    case actionTypes.FETCH_ARTISTS_SONGS_FAIL:
       return false;
     default:
       return state;

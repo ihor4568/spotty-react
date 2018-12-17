67f3f0f7cf72c4902d8361e215e20e3a759483bc
Pavel Baranchuk <pbaranchuk@gmail.com>
2018-12-17 14:59:41 +0200

    Create more tests

--- src/store/actionCreators/__tests__/songs.test.js
+++ src/store/actionCreators/__tests__/songs.test.js
@@ -3,6 +3,8 @@ import * as actionTypes from "../../actionTypes";
 
 import { MusicService } from "../../../services/MusicService";
 
+jest.mock("../../../services/FirebaseService");
+
 describe("songs action creators", () => {
   describe("getArtistSongs", () => {
     it("should return correct action", () => {

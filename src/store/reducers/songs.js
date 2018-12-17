61ca4200f2559cf376d097a3691b176c35265f39
Pavel Baranchuk <pbaranchuk@gmail.com>
2018-12-17 12:45:26 +0200

    KHWE-103: Fix comment

--- src/store/reducers/songs.js
+++ src/store/reducers/songs.js
@@ -5,7 +5,6 @@ export const INITIAL_STATE = [];
 export default function songsReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case actionTypes.FETCH_ALBUM_SONGS_SUCCESS:
-      return action.payload;
     case actionTypes.FETCH_ARTISTS_SONGS_SUCCESS:
       return action.payload;
     default:
